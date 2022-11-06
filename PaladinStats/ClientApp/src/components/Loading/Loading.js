8823cb11dfeff9a5e6a2f7f445942d281cec35e0
DasterPL <daster7@o2.pl>
2022-11-06 15:35:41 +0100

    📝Feature dark loading icon

--- PaladinStats/ClientApp/src/components/Loading/Loading.js
+++ PaladinStats/ClientApp/src/components/Loading/Loading.js
@@ -3,8 +3,8 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Loading.scss';
 
-export default function Loading() {
-  return <div className='loadingBox'>
+export default function Loading({dark}) {
+  return <div className={`loadingBox${dark? ' dark':''}`}>
     <FontAwesomeIcon icon={faSpinner} size="7x" pulse />
     <span>Ładowanie danych</span>
   </div>;
