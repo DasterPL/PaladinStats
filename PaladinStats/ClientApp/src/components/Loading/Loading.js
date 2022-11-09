3c0fbd60697372cf49e12f44f43f524214fc5980
DasterPL <daster7@o2.pl>
2022-11-09 15:00:12 +0100

    📝Feature add progress bar option to loading

--- PaladinStats/ClientApp/src/components/Loading/Loading.js
+++ PaladinStats/ClientApp/src/components/Loading/Loading.js
@@ -3,9 +3,9 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Loading.scss';
 
-export default function Loading({dark}) {
-  return <div className={`loadingBox${dark? ' dark':''}`}>
-    <FontAwesomeIcon icon={faSpinner} size="7x" pulse />
+export default function Loading({ dark, progress }) {
+  return <div className={`loadingBox${dark ? ' dark' : ''}`}>
+    {progress == null ? <FontAwesomeIcon icon={faSpinner} size="7x" pulse /> : <progress value={progress} max={100}></progress>}
     <span>Ładowanie danych</span>
   </div>;
 }
